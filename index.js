9dd72116b292614faf8da061d2788cc7750ec71c
promisingcoder <hiet122230340@Sinaieng.edu.eg>
2024-10-12 06:22:09 +0300

    added UserSchema

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const {GetProducts} =  require("./GetProducts")
 const GetProductById = require("./GetProductById")
 var cors = require('cors')
+const bcrypt = require('bcrypt')
+
 
 conn_string  = process.env.conn_string
 
 const app = express();
 app.use(cors())
-
+app.use(express.json())
 const port = 3000;
 
 app.get("/products", function (req, res) {
@@ -18,6 +20,13 @@ app.get('/products/:id', function(req, res) {
   product =  GetProductById(conn_string,req.params.id).then((data) => res.send(data));
   
 });
+app.post('/user', function (req, res) {    
+  response = {  
+      data:req.body
+  };  
+  console.log(response)
+  res.json(JSON.stringify(response));  
+})  
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
