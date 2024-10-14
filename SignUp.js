5d9803c42e1318f1366f02e60b080bf786f150a0
promisingcoder <hiet122230340@Sinaieng.edu.eg>
2024-10-14 14:04:38 +0300

    update

--- SignUp.js
+++ SignUp.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 const User = require('UserSchema');
 const bcrypt = require('bcrypt');
-router.post('/register', async (req, res) => {
+router.post('/register', async (err,req, res) => {
     try {
     const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
+    if(!userExists){
     const user = new User({ username, password: hashedPassword });
+    }
+    else{
+        throw new Error("User already exists")
+    }
     await user.save();
     res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
