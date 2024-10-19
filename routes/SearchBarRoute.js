3befafe6b02b6542a94712158487cc1b450006e1
promisingcoder <hiet122230340@Sinaieng.edu.eg>
2024-10-19 16:33:05 +0300

    added search route

--- routes/SearchBarRoute.js
+++ routes/SearchBarRoute.js
@@ -2,24 +2,23 @@ const express = require('express');
 const router = express.Router();
 const { Product } = require("../models/ProductSchema"); 
 
-
 router.get("/search", async (req, res) => {
     try {
-      const keyword = req.query.keyword;
-  
-      const products = await Product.find({
-        $or: [
-          { name: { $regex: keyword, $options: "i" } },
-          { description: { $regex: keyword, $options: "i" } },
-          { category: { $regex: keyword, $options: "i" } }
-        ]
-      });
-  
-      res.status(200).json(products);
+        const keyword = req.query.keyword;
+        
+        const products = await Product.find({
+            $or: [
+                { Name: { $regex: keyword, $options: "i" } },
+                { Overview: { $regex: keyword, $options: "i" } },
+                { Category: { $regex: keyword, $options: "i" } }
+            ]
+        });
+        
+        res.status(200).json(products);
     } catch (error) {
-      console.error("Error searching products:", error);
-      res.status(500).json({ error: "Internal server error" });
+        console.error("Error searching products:", error);
+        res.status(500).json({ error: "Internal server error" });
     }
-  });
-module.exports = router
-  
+});
+
+module.exports = router;
