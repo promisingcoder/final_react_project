a3b38db37946bfe0e8fc88fd16299cd43a709e93
promisingcoder <hiet122230340@Sinaieng.edu.eg>
2024-10-23 09:49:55 +0300

    modified add to cart route

--- routes/AddToCartRoute.js
+++ routes/AddToCartRoute.js
@@ -6,10 +6,9 @@ router = express.Router()
  router.post('/AddToCart', verifyToken, (req, res) => {
     res.status(200).json({ message: 'Protected route accessed' });
     try {
-        const {userId, cartItems } = req.body
+        const {cartItems } = req.body
         
-        console.log(req.body)
-        addToCart(userId,cartItems,process.env.conn_string)
+        addToCart(req.userId,cartItems,process.env.conn_string)
     } catch (error) {
         console.log(`Error adding address :  ${error}`)
     }
