bc65d4d905c7f2cc08385af8406be0010dfe7412
promisingcoder <hiet122230340@Sinaieng.edu.eg>
2024-10-23 10:19:11 +0300

    quick fix

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const GetProductById = require("./db_operations/GetProductById")
+const GetProductById = require("../db_operations/GetProductById")
 const router =  express.Router()
 
 router.get('/products/:id', function(req, res) {
